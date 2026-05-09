c0faebd1eb42d2bb2004b94e1a4253d8469d1ce4
ziad-rohayiem <ziad.hesham@ejust.edu.eg>
2026-05-09 20:47:37 +0300

    Adding the external library logic

---
+++ Week-1/Day-2/Task-1/my-library/src/main/java/com/example/ZiadLibrary.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class ZiadLibrary {
+    static {
+        System.out.println("Hello from Ziad's Library!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
