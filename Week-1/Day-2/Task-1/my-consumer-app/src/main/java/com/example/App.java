719e2a4ce5278af1b7161d89177e50bfebedfa5c
ziad-rohayiem <ziad.hesham@ejust.edu.eg>
2026-05-09 20:48:22 +0300

    Adding the consumer App

---
+++ Week-1/Day-2/Task-1/my-consumer-app/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+// import com.example.ZiadLibrary;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int result = ZiadLibrary.add(6, 7);
+        System.out.println("Result: " + result);
+    }
+}
